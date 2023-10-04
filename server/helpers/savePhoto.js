6593520dc76d5fc7e94e65fd387bb730a9d0e4f5
ShrutiSharma77 <shruti.sharma.met21@itbhu.ac.in>
2023-10-04 13:07:41 +0530

    post feed added

--- server/helpers/savePhoto.js
+++
@@ -1,8 +0,0 @@
-const savePhoto = (post, photoEncoded, photoType) => {
-	if (photoEncoded != null) {
-		post.photo = new Buffer.from(photoEncoded, "base64");
-		post.photoType = photoType;
-	}
-};
-
-module.exports = { savePhoto };
